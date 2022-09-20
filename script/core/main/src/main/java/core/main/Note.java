ba0066d649a895f52d962d3cd358c0ebe6755b82
Iver Ringheim <iverri@stud.ntnu.no>
2022-09-20 15:40:19 +0200

    Created Note Class

--- script/core/main/src/main/java/core/main/Note.java
+++ script/core/main/src/main/java/core/main/Note.java
@@ -1,5 +1,36 @@
 package core.main;
 
 public class Note {
+    
+    String title;
+    String text;
+
+    /// Create note with already existing title and text (When loading in already existing notes maybe?)
+    public Note(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    /// Create empty note
+    public Note() {
+        
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 
 }
+
