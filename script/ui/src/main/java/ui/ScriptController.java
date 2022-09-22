29c28eea34da7f61c90841de4806ed8aa65483d8
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2022-09-22 15:41:15 +0200

    add-board-button-loading

--- script/ui/src/main/java/ui/ScriptController.java
+++ script/ui/src/main/java/ui/ScriptController.java
@@ -1,5 +1,64 @@
 package ui;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import core.main.Board;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class ScriptController{
+
+    @FXML
+    private GridPane boardGrid;
     
+    @FXML
+    private AnchorPane boardAnchor;
+
+    @FXML
+    private void initialize() {
+        try {
+            createBoardButtons(getBoardsFromFile());
+        } catch (IOException e) {
+            e.printStackTrace(); }
+    }
+
+    @FXML
+    private void clickBoardButton(ActionEvent ae) throws IOException {
+        
+    }
+    
+    private void createBoardButtons(List<Board> boards) throws IOException {
+        IntStream.range(0, boards.size()).forEach(i -> {
+            Button button = new Button(boards.get(i).getBoardName());
+            button.wrapTextProperty().setValue(true);
+            button.setStyle("-fx-text-alignment: center;");
+            button.setCursor(Cursor.HAND);
+            button.setOnAction((event) -> {
+                try {
+                    clickBoardButton(event);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            button.setMaxWidth(190);
+            boardGrid.add(button, 0, i);
+        });
+        ScrollPane scrollPane = new ScrollPane(boardGrid);
+        scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+        boardAnchor.getChildren().add(scrollPane);
+    }
+
+    private List<Board> getBoardsFromFile() {
+        return Arrays.asList();
+    }
 }
