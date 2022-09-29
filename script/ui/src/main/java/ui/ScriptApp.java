c73d5fcf9089b324f7e6223a00405ac15fd1cd33
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2022-09-29 22:52:45 +0200

    Add multiple user functionality and login screen

--- script/ui/src/main/java/ui/ScriptApp.java
+++ script/ui/src/main/java/ui/ScriptApp.java
@@ -15,7 +15,7 @@ public class ScriptApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Script.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Login.fxml"));
         Parent parent = fxmlLoader.load();
         stage.setScene(new Scene(parent));
         stage.show();
