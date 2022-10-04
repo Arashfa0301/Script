96f642433b04ca1f37f64e52511eb98853d84edc
Iver Ringheim <iver.ringheim@gmail.com>
2022-10-04 14:00:54 +0200

    Populated color map in Note

--- script/core/main/src/main/java/core/main/Note.java
+++ script/core/main/src/main/java/core/main/Note.java
@@ -13,8 +13,20 @@ public class Note {
     private boolean isPinned = false;
     private String color = "white";
     private final Map<String, List<Integer>> selectableColors = Stream.of(
-            new AbstractMap.SimpleImmutableEntry<>("red", Arrays.asList(120, 120, 120)),
-            new AbstractMap.SimpleImmutableEntry<>("blue", Arrays.asList(100, 100, 100)))
+            new AbstractMap.SimpleImmutableEntry<>("red", Arrays.asList(255, 0, 0)),
+            new AbstractMap.SimpleImmutableEntry<>("blue", Arrays.asList(0, 0, 255)),
+            new AbstractMap.SimpleImmutableEntry<>("coral", Arrays.asList(255, 99, 80)),
+            new AbstractMap.SimpleImmutableEntry<>("khaki", Arrays.asList(240, 230, 140)),
+            new AbstractMap.SimpleImmutableEntry<>("aquamarine", Arrays.asList(102, 205, 170)),
+            new AbstractMap.SimpleImmutableEntry<>("sea green", Arrays.asList(32, 178, 170)),
+            new AbstractMap.SimpleImmutableEntry<>("powder blue", Arrays.asList(176, 224, 230)),
+            new AbstractMap.SimpleImmutableEntry<>("yellow", Arrays.asList(255, 255, 0)),
+            new AbstractMap.SimpleImmutableEntry<>("green", Arrays.asList(0, 128, 0)),
+            new AbstractMap.SimpleImmutableEntry<>("orchid", Arrays.asList(218, 112, 214)),
+            new AbstractMap.SimpleImmutableEntry<>("violet red", Arrays.asList(199, 21, 133)),
+            new AbstractMap.SimpleImmutableEntry<>("lemon chiffon", Arrays.asList(255, 250, 205)),
+            new AbstractMap.SimpleImmutableEntry<>("misty rose", Arrays.asList(255, 228, 225)),
+            new AbstractMap.SimpleImmutableEntry<>("alice blue", Arrays.asList(240, 248, 255)))
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
     // Maybe implement checkboxes
