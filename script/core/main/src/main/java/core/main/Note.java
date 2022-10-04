797e2bdf221f65d4a698de6589746f10d9281fdf
Iver Ringheim <iver.ringheim@gmail.com>
2022-10-04 16:01:54 +0200

    tests for core module

--- script/core/main/src/main/java/core/main/Note.java
+++ script/core/main/src/main/java/core/main/Note.java
@@ -12,22 +12,6 @@ public class Note {
     private String title, text;
     private boolean isPinned = false;
     private String color = "white";
-    private final Map<String, List<Integer>> selectableColors = Stream.of(
-            new AbstractMap.SimpleImmutableEntry<>("red", Arrays.asList(255, 0, 0)),
-            new AbstractMap.SimpleImmutableEntry<>("blue", Arrays.asList(0, 0, 255)),
-            new AbstractMap.SimpleImmutableEntry<>("coral", Arrays.asList(255, 99, 80)),
-            new AbstractMap.SimpleImmutableEntry<>("khaki", Arrays.asList(240, 230, 140)),
-            new AbstractMap.SimpleImmutableEntry<>("aquamarine", Arrays.asList(102, 205, 170)),
-            new AbstractMap.SimpleImmutableEntry<>("sea green", Arrays.asList(32, 178, 170)),
-            new AbstractMap.SimpleImmutableEntry<>("powder blue", Arrays.asList(176, 224, 230)),
-            new AbstractMap.SimpleImmutableEntry<>("yellow", Arrays.asList(255, 255, 0)),
-            new AbstractMap.SimpleImmutableEntry<>("green", Arrays.asList(0, 128, 0)),
-            new AbstractMap.SimpleImmutableEntry<>("orchid", Arrays.asList(218, 112, 214)),
-            new AbstractMap.SimpleImmutableEntry<>("violet red", Arrays.asList(199, 21, 133)),
-            new AbstractMap.SimpleImmutableEntry<>("lemon chiffon", Arrays.asList(255, 250, 205)),
-            new AbstractMap.SimpleImmutableEntry<>("misty rose", Arrays.asList(255, 228, 225)),
-            new AbstractMap.SimpleImmutableEntry<>("alice blue", Arrays.asList(240, 248, 255)))
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
     // Maybe implement checkboxes
 
@@ -69,10 +53,33 @@ public class Note {
     public void setColor(String color) {
         if (isValidColor(color)) {
             this.color = color;
-            return;
+        } else {
+            throw new IllegalArgumentException("This is not a valid color");
         }
-        throw new IllegalArgumentException("This is not a valid color");
+    }
 
+    public Map<String, List<Integer>> getSelectableColors() {
+        return Stream.of(
+                new AbstractMap.SimpleImmutableEntry<>("red", Arrays.asList(255, 0, 0)),
+                new AbstractMap.SimpleImmutableEntry<>("blue", Arrays.asList(0, 0, 255)),
+                new AbstractMap.SimpleImmutableEntry<>("coral", Arrays.asList(255, 99, 80)),
+                new AbstractMap.SimpleImmutableEntry<>("khaki", Arrays.asList(240, 230, 140)),
+                new AbstractMap.SimpleImmutableEntry<>("aquamarine", Arrays.asList(102, 205, 170)),
+                new AbstractMap.SimpleImmutableEntry<>("sea green", Arrays.asList(32, 178, 170)),
+                new AbstractMap.SimpleImmutableEntry<>("powder blue", Arrays.asList(176, 224, 230)),
+                new AbstractMap.SimpleImmutableEntry<>("yellow", Arrays.asList(255, 255, 0)),
+                new AbstractMap.SimpleImmutableEntry<>("green", Arrays.asList(0, 128, 0)),
+                new AbstractMap.SimpleImmutableEntry<>("orchid", Arrays.asList(218, 112, 214)),
+                new AbstractMap.SimpleImmutableEntry<>("violet red", Arrays.asList(199, 21, 133)),
+                new AbstractMap.SimpleImmutableEntry<>("lemon chiffon", Arrays.asList(255, 250, 205)),
+                new AbstractMap.SimpleImmutableEntry<>("misty rose", Arrays.asList(255, 228, 225)),
+                new AbstractMap.SimpleImmutableEntry<>("alice blue", Arrays.asList(240, 248, 255)),
+                new AbstractMap.SimpleImmutableEntry<>("white", Arrays.asList(255, 255, 255)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public List<Integer> getColorValues() {
+        return getSelectableColors().get(getColor());
     }
 
     public String getColor() {
@@ -80,7 +87,7 @@ public class Note {
     }
 
     private boolean isValidColor(String color) {
-        return selectableColors.containsKey(color);
+        return getSelectableColors().containsKey(color);
     }
 
     @Override
