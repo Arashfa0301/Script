2feffe8eec3433be80a06bf944f23368f9f42735
Mattias Tofte <mattiastofte@gmail.com>
2022-11-01 17:29:52 +0100

    Add structure for the api and its endpoints

---
+++ script/springboot/server/src/main/java/springboot/server/ApiService.java
@@ -0,0 +1,13 @@
+package springboot.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiService.class, args);
+    }
+
+}
