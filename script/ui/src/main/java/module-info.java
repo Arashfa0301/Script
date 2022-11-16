2dcd691b48e7e779e25b1ec7a7c81645734ef17a
Mattias Tofte <mattiastofte@gmail.com>
2022-11-16 19:50:53 +0100

    Add dependencies

--- script/ui/src/main/java/module-info.java
+++ script/ui/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module script.ui {
-    requires java.sql;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.net.http;
-    requires script.core.main;
-    requires script.data;
+  requires com.fasterxml.jackson.databind;
 
-    opens ui to javafx.graphics, javafx.fxml;
-  }
+  requires java.sql;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires java.net.http;
+  requires script.core.main;
+  requires script.data;
+
+  opens ui to javafx.graphics, javafx.fxml;
+}
