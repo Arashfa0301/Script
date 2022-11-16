34061b3431f74dc2c25ab50a36c238412787357f
Mattias Tofte <mattiastofte@gmail.com>
2022-11-16 17:45:59 +0100

    Make password serialisable both ways

--- script/core/main/src/main/java/core/main/User.java
+++ script/core/main/src/main/java/core/main/User.java
@@ -14,7 +14,6 @@ import java.util.regex.Pattern;
 public class User {
 
     private String username;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String firstName;
     private String lastName;
